1dd659c3f48e60fcef3975bb51f58db93ca124c0
tommaso lauria <tommaso.lauria9@gmail.com>
2020-11-10 12:45:30 +0100

    definitivo

--- DaLetteraASCII/Program.cs
+++ DaLetteraASCII/Program.cs
@@ -11,7 +11,7 @@ namespace DaLetteraASCII
             ushort carattereInAscii = 0;
             carattereInAscii = carattere;
             string numero = Convert.ToString(carattereInAscii, 2).PadLeft(16, '0');
-            Console.WriteLine($"Carattere {carattere} - Carattere in ASCII {carattereInAscii}- NumeroBinario {numero}");
+            Console.WriteLine($"Carattere {carattere} - Carattere in ASCII {carattereInAscii} - NumeroBinario {numero}");
 
         }
     }
