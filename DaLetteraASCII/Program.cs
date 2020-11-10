43eed47def13fda875a617798673d72bca907f85
tommaso lauria <tommaso.lauria9@gmail.com>
2020-11-10 12:26:31 +0100

    Add project files.

---
+++ DaLetteraASCII/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DaLetteraASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci una lettera di cui vuoi sapere in numero in ASCII");
+            char carattere = Convert.ToChar(Console.ReadLine());
+            ushort carattereInAscii = 0;
+            carattereInAscii = carattere;
+            Console.WriteLine($"Carattere {carattere} - Carattere in ASCII {carattereInAscii}");
+        }
+    }
+}
